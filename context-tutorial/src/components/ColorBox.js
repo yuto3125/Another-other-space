f60418ef2e04a053d5a354de18b5782a2f6bf468
yuto3125 <51585533+yuto3125@users.noreply.github.com>
2020-08-01 15:31:14 +0900

    with hooks

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import ColorContext from '../contexts/Color';
-import { ColorConsumer } from '../contexts/Color';
+import ColorContext from '../contexts/Color';
+// import ColorContext, { ColorConsumer } from '../contexts/Color';
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{ width: '64px', height: '64px', background: state.color }}
-          />
-          <div
-            style={{
-              width: '32px',
-              height: '32px',
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    // {/*<ColorConsumer> */}
+    // {/* {({ state }) => ( */}
+    <>
+      <div style={{ width: '64px', height: '64px', background: state.color }} />
+      <div
+        style={{
+          width: '32px',
+          height: '32px',
+          background: state.subcolor,
+        }}
+      />
+    </>
+    //  {/* )} */}
+    // {/* </ColorConsumer> */}
   );
 };
 
